7160a4495c9025145ea47577f7587f47180c4b39
nigel <nigel@gmail.com>
2023-03-16 14:14:27 -0700

    all

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
+#include <stdio.h>
 #include <unistd.h>
 /**
- * main - A c program that prints a line to the standard error
- * Return: 1 (Success)
+ * main -Entry
+ * Return: 1
  */
 int main(void)
 {
-	char quo[] = ("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(1, quo, 59);
-	return (1);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1)
 }
