39a81c10da525197de4a688c5dd52b4211d32f3a
nigel <nigel@gmail.com>
2023-03-20 19:11:56 -0700

    aa

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,11 +10,12 @@ int main(void)
 	int i;
 
 	for (i = 0 ; i < 10 ; i++)
-	{	if (i == 9)
+
+	{
+		if (i == 9)
 			putchar(i + '0');
 		else
 		{
-		
 			putchar(i + '0');
 			putchar(',');
 			putchar(' ');
