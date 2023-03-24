281bc3e2e1e412e3abdc9a6952c3eb46ca5bb98b
nigel <nigel@gmail.com>
2023-03-23 18:29:44 -0700

    AKK

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul -  multplies two numbers
+ * @a: first parameter
+ * @b: second parameter
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+int result = a * b;
+return (result);
+}
