52eed27a1c69257682b7efffa0c585baefd1564c
nigel <nigel@gmail.com>
2023-03-23 18:07:57 -0700

    aal

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for lowercase character
+ * @c: parameter to be checked
+ * Return: 1 for uppercase 0 for anything else
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && <= 90)
+	{
+		return (1);
+	}
+		return (0);
+}
