5cd346e66779e6d9edcf6fede1d9b7c6806a02e5
nigel <nigel@gmail.com>
2023-03-23 17:19:31 -0700

    ad

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if(n >= 0 && n <= 98)
+	{
+		for (i = n ; i <= 98 ; i++)
+		{
+		printf("%d, ", 1);
+		}
+		printf(
+	}
+}
