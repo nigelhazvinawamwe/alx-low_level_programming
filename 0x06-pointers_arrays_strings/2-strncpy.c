628aed319f7eb2133850a1497423c6297fa027d7
nigel <nigel@gmail.com>
2023-03-31 14:38:56 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,22 +1,27 @@
-#include "main.h"                                                                                                                    
+#include "main.h"
 
-/**                                                                                                                                  
- * _strcmp - Compares pointers to two strings.                                                                                       
- * @s1: A pointer to the first string to be compared.                                                                                
- * @s2: A pointer to the second string to be compared.                                                                               
- *                                                                                                                                   
- * Return: If str1 < str2, the negative difference of the first unmatched char                                                       
- *         If str1 == str2, 0.                                                                                                       
- *         If str1 > str2, the positive difference of the first unmatched char                                                       
- */                                                                                                                                  
-int _strcmp(char *s1, char *s2)                                                                                                      
-{                                                                                                                                    
-	while (*s1 && *s2 && *s1 == *s2)                                                                                             
-	{                                                                                                                            
-		s1++;                                                                                                                
-		s2++;                                                                                                                
-	}                                                                                                                            
+/**
+ * _strncpy - Copies at most an inputted number
+ *            of bytes from string src into dest.
+ * @dest: The buffer storing the string copy.
+ * @src: The source string.
+ * @n: The maximum number of bytes to copied from src.
+ *
+ * Return: A pointer to the resulting string dest.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index = 0, src_len = 0;
 
-	return (*s1 - *s2);                                                                                                          
+	while (src[index++])
+		src_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
+
+	for (index = src_len; index < n; index++)
+		dest[index] = '\0';
+
+	return (dest);
 }
 
