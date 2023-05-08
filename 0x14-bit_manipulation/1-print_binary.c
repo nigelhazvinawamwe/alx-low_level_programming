893042fcf76d8c116d100df24cadcc618717175c
nigel <nigel@gmail.com>
2023-05-07 17:24:44 -0700

    binary_to_uint

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,27 +1,31 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_binary - prints the binary representation of a number
  * @n: the number to print in binary form
  *
  * Return: void
+ *
  */
+
 void print_binary(unsigned long int n)
 {
-	unsigned long int mask = 1;
+	int i, count = 0;
+	unsigned long int current;
 
-	/* Find the most significant bit in n */
-	while ((mask << 1) <= n)
-		mask <<= 1;
-
-	/* Print the binary representation of n */
-	while (mask != 0)
+	for (i = 63; i >= 0; i--)
 	{
-		if ((n & mask) == 0)
-			putchar('0');
-		else
-			putchar('1');
-		mask >>= 1;
+		current = n >> i;
+
+		if (current & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
 	}
+	if (!count)
+		_putchar('0');
 }
+
